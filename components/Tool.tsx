9657820a402127d3a4ed305fa5e6e56ef20affda
Sanusi Hassan <sanusihassan122@gmail.com>
2024-03-13 20:37:26 +0100

    added validation ondrop

--- components/Tool.tsx
+++ components/Tool.tsx
@@ -15,6 +15,7 @@ import { useFileStore } from "../src/file-store";
 import { FileInputForm } from "./Tool/FileInputForm";
 import DownloadFile from "./DownloadFile";
 import EditPage from "./EditPage";
+import { validateFiles } from "@/src/utils";
 
 export type errorType = {
   response: {
@@ -75,8 +76,11 @@ const Tool: React.FC<ToolProps> = ({
   // const [endpoint, setEndpoint] = useState("");
   // drag and drop input handling
   const onDrop = useCallback((acceptedFiles: File[]) => {
-    setFiles(acceptedFiles);
-    handleHideTool();
+    const isValid = validateFiles(acceptedFiles, data.type, errors, dispatch, { path: data.to });
+    if (isValid) {
+      setFiles(acceptedFiles);
+      handleHideTool();
+    }
   }, []);
   const { getRootProps, isDragActive } = useDropzone({ onDrop });
 
