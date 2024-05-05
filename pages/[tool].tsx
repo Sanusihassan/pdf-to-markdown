e5750c07900b3edd99e84630de15841e4e05fa1b
Sanusi Hassan <sanusihassan122@gmail.com>
2024-05-06 01:39:10 +0200

    added favicon for en

--- pages/[tool].tsx
+++ pages/[tool].tsx
@@ -60,6 +60,7 @@ export default ({ item }: { item: _tool["PDF_to_Markdown"] }) => {
           }}
         />
         <meta name="description" content={item.description} />
+        <link rel="icon" type="image/svg+xml" href="/images/icons/logo.svg" />
         <OpenGraph
           ogUrl={`https://www.pdfequips.com${item.to}`}
           ogDescription={item.description}
